0af74b6f21a6168ec90657f6e5b61b8713bd8e44
nikguscode <nikguscode@gmail.com>
2024-09-23 01:08:57 +0500

    Finished without documentation and tests

--- src/main/java/com/nikguscode/calculator/calculator/controller/Constants.java
+++ src/main/java/com/nikguscode/calculator/calculator/controller/Constants.java
@@ -7,12 +7,10 @@ public class Constants {
     public static final String DATE_PATTERN = "dd-MM-yyyy";
     public static final String UNDEFINED_BAD_REQUEST = "Некорректный запрос, обратитесь к документации по ссылке: " +
             "https://github.com/nikguscode/VacationCalculator";
-
     public static final String DATE_PERIOD_BAD_REQUEST = "Некорректный период между датами или продолжительность отпуска. " +
             "Обратитесь к документации по ссылке: https://github.com/nikguscode/VacationCalculator";
     public static final String URL_BAD_REQUEST = "Обратитесь к документации по ссылке: " +
             "https://github.com/nikguscode/VacationCalculator";
-
     public static final String DATE_BAD_REQUEST = "Указана некорректная дата. Обратитесь к документации по ссылке: " +
             "https://github.com/nikguscode/VacationCalculator";
 }
