334f0bf0650ad361aade8a5dbf2033a8d7fa8e3e
nikguscode <nikguscode@gmail.com>
2024-09-23 19:52:37 +0500

    Test added, finish

--- src/test/java/com/nikguscode/calculator/calculator/PayoutCalculatorTest.java
+++ src/test/java/com/nikguscode/calculator/calculator/PayoutCalculatorTest.java
@@ -1,7 +1,6 @@
 package com.nikguscode.calculator.calculator;
 
 import com.nikguscode.calculator.calculator.exception.InvalidValueException;
-import com.nikguscode.calculator.calculator.model.HolidaysDateStorage;
 import com.nikguscode.calculator.calculator.model.calculator.PayoutCalculator;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Assertions;
@@ -11,31 +10,14 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
-import static java.util.Map.entry;
-
+/**
+ * {@link PayoutCalculatorTest#test1()}, {@link PayoutCalculatorTest#test2()}: проверяют стратегию без учёта праздничных дней <br/>
+ * {@link PayoutCalculatorTest#test3()}, {@link PayoutCalculatorTest#test4()}: проверяют стратегию с учётом праздничных дней
+ */
 @Log4j2
 @SpringBootTest
 public class PayoutCalculatorTest {
-    static Map<LocalDate, Boolean> holidaysList = Map.ofEntries(
-            entry(LocalDate.of(2024, 5, 9), false), // holiday
-            entry(LocalDate.of(2024, 5, 2), true) // payout day
-    );
-    static List<Integer> vacationDurationList = new ArrayList<>(
-            List.of(
-                    6
-            )
-    );
-    static List<Double> salaryList = new ArrayList<>();
-
-    static {
-        salaryList.add(50000.0);
-        salaryList.add(120000.0);
-    }
-
     PayoutCalculator calculator;
 
     public PayoutCalculatorTest(@Qualifier("localPayoutCalculator") PayoutCalculator calculator) {
@@ -43,38 +25,36 @@ public class PayoutCalculatorTest {
     }
 
     @Test
-    @DisplayName("Payout day test")
-    void isPayoutDay() {
-        log.info("TRUE: Payable, FALSE: Non-payable");
-
-        for (Map.Entry<LocalDate, Boolean> date : holidaysList.entrySet()) {
-            Assertions.assertEquals(date.getValue(), HolidaysDateStorage.isPayableDay(date.getKey()));
-            log.info(
-                    "Date: {}, Excepted value: {}, Actual value: {}",
-                    date.getKey(),
-                    date.getValue(),
-                    HolidaysDateStorage.isPayableDay(date.getKey())
-            );
-        }
+    @DisplayName("Correct values | Duration only")
+    void test1() throws InvalidValueException {
+        double salary = 25000.0;
+        int vacationDuration = 5;
+        Assertions.assertEquals(4266, calculator.calculate(salary, vacationDuration));
     }
-
 
     @Test
-    @DisplayName("Calculate without date")
-    void calculateWithoutDate() throws InvalidValueException {
-        Assertions.assertEquals(10239, calculator.calculate(salaryList.get(0), vacationDurationList.get(0)));
-        log.info("Excepted: {}, actual: {}", 10239, calculator.calculate(
-                        salaryList.get(0),
-                        vacationDurationList.get(0)
-                )
-        );
+    @DisplayName("Incorrect values | Duration only")
+    void test2() {
+        double salary = 0;
+        int vacationDuration = 5;
+        Assertions.assertThrows(InvalidValueException.class, () -> calculator.calculate(salary, vacationDuration));
     }
 
-    void calculateWithOnlyStartDate() {
-
+    @Test
+    @DisplayName("Correct values | Duration and start date")
+    void test3() throws InvalidValueException {
+        double salary = 50000.0;
+        int vacationDuration = 9;
+        LocalDate startVacationDate = LocalDate.of(2025, 1, 1);
+        Assertions.assertEquals(1706, calculator.calculate(salary, vacationDuration, startVacationDate));
     }
 
-    void calculateWithAllDate() {
-
+    @Test
+    @DisplayName("Incorrect values | Duration and start date")
+    void test4() {
+        double salary = 50000.0;
+        int vacationDuration = -2;
+        LocalDate startVacationDate = LocalDate.of(2025, 1, 1);
+        Assertions.assertThrows(InvalidValueException.class, () -> calculator.calculate(salary, vacationDuration, startVacationDate));
     }
 }
